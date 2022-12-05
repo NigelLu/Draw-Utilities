c55c883e524b00380f4bf01fe1542b77c9b38b69
Nigel <nigellu@outlook.com>
2022-12-06 07:34:06 +0800

    doc: added docstring to functions in convert.py

--- functions/convert.py
+++ functions/convert.py
@@ -8,6 +8,18 @@ from einops import rearrange
 
 
 def detach(draw_info_dict: dict, keys_to_process: list) -> None:
+    """Detach tensors in draw_info_dict
+
+    Detach the tensors (in place) in draw_info_dict based on the keys given by keys_to_process
+
+    Args:
+        draw_info_dict {dict}: a python dictionary with values being torch.Tensor
+        keys_to_process {list}: a python list with values being (typically) strings or integers, 
+                                specifying the keys in draw_info_dict to be processed
+
+    Returns:
+        None
+    """
     # * double check for argument type
     assert type(
         draw_info_dict) == dict, f"Expect draw_info_dict argument to be a dict, got {type(draw_info_dict)} instead"
@@ -28,6 +40,22 @@ def detach(draw_info_dict: dict, keys_to_process: list) -> None:
 
 
 def to_float32(draw_info_dict: dict, keys_to_process: list, in_place: bool = True) -> dict:
+    """Convert tensors to torch.float32 in draw_info_dict
+
+    Convert the tensors to torch.float32 in draw_info_dict based on the keys given by keys_to_process
+
+    Args:
+        draw_info_dict {dict}: a python dictionary with values being torch.Tensor
+        keys_to_process {list}: a python list with values being (typically) strings or integers, 
+                                specifying the keys in draw_info_dict to be processed
+        in_place {bool}: True/False argument, specifying whether to convert the tensors in an in-place manner
+
+    Returns:
+        A dict with the keys in keys_to_process, with values being
+            None (if in_place is True)
+                OR
+            torch.Tensor (if in_place is False), these tensors of type torch.float32 and are copies of tensors in draw_info_dict
+    """
     # * double check for argument type
     assert type(
         in_place) == bool, f"Expect keys_to_process argument to be a bool, got {type(in_place)} instead"
@@ -41,11 +69,12 @@ def to_float32(draw_info_dict: dict, keys_to_process: list, in_place: bool = Tru
     try:
         # * create a new info dict for not in_place conversion
         new_draw_info_dict = dict([(key, None)
-                                  for key in draw_info_dict.keys()])
+                                  for key in keys_to_process])
 
         for key in keys_to_process:
-            new_draw_info_dict[key] = new_draw_info_dict[key].clone().type(
-                torch.float32)
+            if not in_place:
+                new_draw_info_dict[key] = new_draw_info_dict[key].clone().type(
+                    torch.float32)
             if in_place:
                 draw_info_dict[key] = draw_info_dict[key].type(torch.float32)
 
@@ -59,6 +88,23 @@ def to_float32(draw_info_dict: dict, keys_to_process: list, in_place: bool = Tru
 
 
 def tocpu_and_asNumpy(draw_info_dict: dict, keys_to_process: list, in_place: bool = True) -> dict:
+    """Send tensors to CPU and convert them from torch.Tensor to Numpy
+
+    Send the tensors in draw_info_dict to CPU and convert them from torch.Tensor to Numpy based on the keys given by keys_to_process
+
+    Args:
+        draw_info_dict {dict}: a python dictionary with values being torch.Tensor
+        keys_to_process {list}: a python list with values being (typically) strings or integers, 
+                                specifying the keys in draw_info_dict to be processed
+        in_place {bool}: True/False argument, specifying whether to convert the tensors in an in-place manner
+
+    Returns:
+        A dict with the keys in keys_to_process, with values being
+            None (if in_place is True)
+                OR
+            torch.Tensor (if in_place is False), these tensors are copies of tensors in draw_info_dict,
+                which got sent to CPU and converted to Numpy
+    """
     # * double check for argument type
     assert type(
         in_place) == bool, f"Expect keys_to_process argument to be a bool, got {type(in_place)} instead"
@@ -73,11 +119,12 @@ def tocpu_and_asNumpy(draw_info_dict: dict, keys_to_process: list, in_place: boo
     try:
         # * create a new info dict for not in_place conversion
         new_draw_info_dict = dict([(key, None)
-                                  for key in draw_info_dict.keys()])
+                                  for key in keys_to_process])
 
         for key in keys_to_process:
-            new_draw_info_dict[key] = new_draw_info_dict[key].clone(
-            ).cpu().numpy()
+            if not in_place:
+                new_draw_info_dict[key] = new_draw_info_dict[key].clone(
+                ).cpu().numpy()
             if in_place:
                 draw_info_dict[key] = draw_info_dict[key].cpu().numpy()
 
@@ -91,6 +138,25 @@ def tocpu_and_asNumpy(draw_info_dict: dict, keys_to_process: list, in_place: boo
 
 
 def apply_mask(draw_info_dict: dict, image_to_mask_map: dict, MASK_WEIGHT: float, GAMMA_SCALAR: float, rearrange_str: str = None) -> dict:
+    """Apply a red mask on top of the images
+
+    Apply a red mask on top of the images, with the keys of masks and images specified by an image_to_mask_map
+
+    Args:
+        draw_info_dict {dict}: a python dictionary with values being torch.Tensor
+        image_to_mask_map {dict}: a python dictionary specifying an image to mask key mapping (corresponding to the keys in draw_info_dict), 
+                                  as following
+                                    {
+                                        <image_key>: <mask_key>,
+                                    }
+        MASK_WEIGHT {float}: a float in the interval (0, 1) specifying the weight of the mask (image weight will be calculated using 1-MASK_WEIGHT)
+        GAMMA_SCALAR {float}: the scalar to apply on the masked image. This is the same argument used as in cv2.addWeighted()
+        rearrange_str {str}: a string in the form of "c h w -> h w c", for rearranging the image tensor using einops.rearrange function
+
+    Returns:
+        A dict with the sames keys as in image_mask_map, with values being
+            Numpy.Array, these are of shape (h, w, 3) and stores the masked image
+    """
     # * double check for argument type
     assert type(
         rearrange_str) == str, f"Expect rearrange_str argument to be a str, got {type(rearrange_str)} instead"
